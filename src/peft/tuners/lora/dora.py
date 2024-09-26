96cc89a4b961d654385ed4a1b2758ec7d2c76160
sayat <sayt9360@gmail.com>
2024-09-27 04:32:15 +0900

    2

--- src/peft/tuners/lora/dora.py
+++ src/peft/tuners/lora/dora.py
@@ -93,7 +93,7 @@ class DoraLinearLayer(nn.Module):
         mag_norm_scale = (magnitude / weight_norm)
         print("magnorm1")
         print(mag_norm_scale.shape)
-        mag_norm_scale = mag_norm_scale.view(weight.shape[1], -1)
+        mag_norm_scale = mag_norm_scale.view(1, -1)
         print("magnorm2")
         print(mag_norm_scale.shape)
         # mag_norm_scale = mag_norm_scale.norm(dim=1, keepdim=True)
